2d40177a62b58a56b9399e5fc01918a3cc7ffdc7
rope-park <kelly6789@naver.com>
2025-10-01 22:03:43 +0900

    Feat: 편리한 사용을 위한 education 인덱스 설정

---
+++ apps/backend/src/features/education/index.ts
@@ -0,0 +1,5 @@
+// Education Feature Exports
+export * from './education.controller';
+export * from './education.service';
+export * from './education.repository';
+export * from './education.routes';
